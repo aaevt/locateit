43a8c46fe22b57d3afa2af5fffcef9492b87deaf
aaevt <aaevtikhiev@gmail.com>
2025-05-03 18:18:48 +0300

    fix ui

--- src/app/constructor/page.tsx
+++ src/app/constructor/page.tsx
@@ -8,30 +8,38 @@ import Toolbar from "@/components/constructor/Toolbar";
 import Shapesbar from "@/components/constructor/Shapesbar";
 import Activebar from "@/components/constructor/Activebar";
 import Floorsbar from "@/components/constructor/Floorsbar";
-import Layersbar from "@/components/constructor/Layersbar";
 import ImportExportBar from "@/components/constructor/ImportExportBar";
 
 const ConstructorPage: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen bg-white dark:bg-black">
       <Header />
-      <Toolbar />
-      <div className="flex flex-1 pt-20 overflow-hidden">
+      <div className="flex flex-col p-20 overflow-hidden">
+        <div className="flex flew-row justify-center">
+        <Toolbar />
+
         <div className="flex flex-col h-full px-4 gap-4">
           <Shapesbar />
         </div>
-        <div className="flex-1 flex flex-col overflow-hidden">
-          <div className="flex-1 flex items-center justify-center p-4">
+
+        <div className="flex flex-col justify-center overflow-hidden">
+          <div className="flex items-center justify-center">
             <Canvas />
           </div>
-          <div className="px-4 pb-4">
+
+          <div className="pt-4 ">
             <ImportExportBar />
           </div>
+
         </div>
+
         <div className="flex flex-col h-full px-4 gap-4">
           <Floorsbar />
           <Activebar />
         </div>
+
+        </div>
+
       </div>
       <Footer />
     </div>
